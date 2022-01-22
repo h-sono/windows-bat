e16520d2f7dae78919e44e78727c5e516338bfaf
root <root@DESKTOP-I8QEOFF.localdomain>
2022-01-22 21:22:34 +0900

    [update]:verify.bat修正

--- verify.bat
+++ verify.bat
@@ -1,15 +1,30 @@
 @echo off
+chcp 65001
 rem 再起動後に自動実行するバッチ作成⇒RDP WrapperのListenが無効になった際の対応バッチ
 
 rem 以下、再起動後に自動実行するコマンド
-rem cd /d %~dp0
-rem if "%1" neq "" goto :%1
-rem "再起動を要求される処理"
-rem reg add "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\RunOnce" /v "再起動後実行" /t REG_SZ /d "%~0 AfterReboot1" /f
-rem exit
-rem :AfterReboot1
-rem "再起動後に行いたい処理"　←ここに実際の処理を入れる
-rem exit
+cd /d %~dp0
+if "%1" neq "" goto :%1
+"再起動を要求される処理"
+reg add "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\RunOnce" /v "再起動後実行" /t REG_SZ /d "%~0 AfterReboot1" /f
+exit
+:AfterReboot1
+rem 古いrdpwrap.ini削除
+del C:\Users\sonob\verify_file_delete_ok\rdpwrap.ini
+
+rem rdpwrap.iniファイルを指定URLからインストール
+START /WAIT bitsadmin /TRANSFER FILEGET https://raw.githubusercontent.com/affinityv/INI-RDPWRAP/master/rdpwrap.ini C:\Users\sonob\verify_file_delete_ok\rdpwrap.ini
+
+rem 再起動
+SET /P selected="再起動しますか(y/n)？"
+if /i "%selected%"=="y" (goto :yes)
+if /i "%selected%"=="yes" (goto :yes)
+exit 0
+
+:yes
+shutdown -r -t 0
+
+exit
 
 rem ファイルコピー＆ペースト
 rem type nul > C:\Users\sonob\verify_file_delete_ok\hoge.txt
@@ -22,15 +37,15 @@ rem rdpwrap.iniファイルを指定URLからインストール
 rem START /WAIT bitsadmin /TRANSFER FILEGET https://raw.githubusercontent.com/affinityv/INI-RDPWRAP/master/rdpwrap.ini C:\Users\sonob\verify_file_delete_ok\rdpwrap.ini
 
 rem 再起動
-SET /P selected="再起動しますか(y/n)？"
-if /i "%selected%"=="y" (goto :yes)
-if /i "%selected%"=="yes" (goto :yes)
-exit 0
+rem SET /P selected="再起動しますか(y/n)？"
+rem if /i "%selected%"=="y" (goto :yes)
+rem if /i "%selected%"=="yes" (goto :yes)
+rem exit 0
 
-:yes
-shutdown -r -t 0
+rem :yes
+rem shutdown -r -t 0
 
-exit 0
+rem exit 0
 
 rem フォルダ開く
 rem START C:\Users\sonob\verify_file_delete_ok
--- verify2.bat
+++ verify2.bat
@@ -1,11 +1,11 @@
 @echo off
-chcp 65001
+rem chcp 65001
 SET /P selected="再起動しますか(y/n)？"
 if /i "%selected%"=="y" (goto :yes)
 if /i "%selected%"=="yes" (goto :yes)
-rem exit 0
+exit 0
 
 :yes
 shutdown -r -t 0
 
-rem exit 0
+exit 0
